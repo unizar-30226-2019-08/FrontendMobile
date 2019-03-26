06844bf6efc18882a1b735c6205e3f127af71cc0
josman231 <josman.romero@gmail.com>
2019-03-26 21:08:48 +0100

    falta documentar

--- lib/widgets/tablaValoracion.dart
+++ lib/widgets/tablaValoracion.dart
@@ -1,53 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating/flutter_rating.dart';
 //import 'package:smooth_star_rating/smooth_star_rating.dart'; 
 
-class EstrellasValoracion extends StatefulWidget {
-  EstrellasValoracion();
-
-  _EstrellasValoracionState createState() => _EstrellasValoracionState();
-  
-}
-typedef void RatingChangeCallback(double rating);
-class _EstrellasValoracionState extends State<EstrellasValoracion>{
-    
-    //_EstrellasValoracionState({this.color,this.valoracion});
-
-final Color color;
- final int estrellas;
-  final double valoracion;
-  final RatingChangeCallback onRatingChanged;
-
-  _EstrellasValoracionState({this.estrellas = 5, this.valoracion = .0, this.onRatingChanged, this.color});
-
-  Widget buildStar(BuildContext context, int index) {
-    Icon icon;
-    if (index >= valoracion) {
-      icon = new Icon(
-        Icons.star_border,
-        color: Theme.of(context).buttonColor,
-      );
-    }
-    else if (index > valoracion - 1 && index < valoracion) {
-      icon = new Icon(
-        Icons.star_half,
-        color: color ?? Theme.of(context).primaryColor,
-      );
-    } else {
-      icon = new Icon(
-        Icons.star,
-        color: color ?? Theme.of(context).primaryColor,
-      );
-    }
-    return new InkResponse(
-      onTap: onRatingChanged == null ? null : () => onRatingChanged(index + 1.0),
-      child: icon,
-    );
-  }
-  @override
-  Widget build(BuildContext context) {
-    return new Row(children: new List.generate(estrellas, (index) => buildStar(context, index)));
-  }
-}
 
 /*
 @override
@@ -70,17 +24,138 @@ final Color color;
 }*/
 
 
+typedef OnDelete();
+class ValorationCard extends StatefulWidget {
+  final String coment;
+   
+  final OnDelete onDelete;
+   final state= _ValorationCardState();
+   ValorationCard({this.coment,this.onDelete});
+ // ValorationCard();
+  _ValorationCardState createState() => state;
+  bool isValid()=>state.validate();
+  
+}
 
-class TablaValoracion extends StatelessWidget{
-  final String comentario;
-  final String texto;
-  TablaValoracion({Key key,
-    this.comentario,this.texto}): super(key: key);
+class _ValorationCardState extends State<ValorationCard>{
+   double rating=3.0;
+    final _formKey = GlobalKey<FormState>();
+  _ValorationCardState();
 
 
 @override
   Widget build(BuildContext context) {
-    return Card(
+    return Padding(
+      padding: EdgeInsets.all(8),
+      child:Card(
+        child:Form(
+          key:_formKey,
+         child: Column(
+           mainAxisSize:MainAxisSize.min ,
+          children: <Widget>[
+            AppBar(
+              title:Text( "Valora al usuario",),
+              centerTitle: true,
+              backgroundColor: Colors.pink,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [Text("¿Qué te ha parecido este usuario?",
+                        style: TextStyle(height: 1.0,fontSize: 20))],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child:TextFormField(
+                initialValue: "Danos tu opinion",
+                validator: (value) {
+                  if (value.isEmpty) {
+                      return 'No puedes dejar este campo vacio';
+                    }
+                  },
+
+              )
+            ),
+             Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child:Row(
+              
+              children:[new StarRating(
+               size: 25.0,
+                rating: rating,
+                color: Colors.orange,
+                  borderColor: Colors.grey,
+                starCount:5,
+                onRatingChanged: (rating) => setState(
+                    () {
+                      this.rating = rating;
+                    },
+                  ),
+            ),
+          ]
+          ),
+          ),
+
+
+
+
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: IconButton(
+                icon:Icon(Icons.check_circle) ,
+                iconSize: 50,
+                color: Colors.green,
+                onPressed: () {
+                // Validate will return true if the form is valid, or false if
+                // the form is invalid.
+                  if (_formKey.currentState.validate()) {
+                  // If the form is valid, we want to show a Snackbar
+                  Scaffold.of(context)
+                      .showSnackBar(SnackBar(content: Text('Processing Data')));
+                  }
+                },
+              //child: Text("Confirmar"),
+            ),
+            ),
+
+         
+
+          ]
+            )
+
+        ),
+      )
+    );
+  }
+
+  bool validate(){
+    var valid=_formKey.currentState.validate();
+    if(valid)_formKey.currentState.save();
+    return valid;
+  }
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*return Card(
+      elevation: 4.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
       color:Colors.pink,
       child:Column(
         children:[
@@ -88,12 +163,73 @@ class TablaValoracion extends StatelessWidget{
             children: [Text("¿Qué te ha parecido este usuario?")],
           ),
           Row(
-            children:[EstrellasValoracion()],
+            children:[new StarRating(
+              size: 25.0,
+              rating: rating,
+              color: Colors.orange,
+              borderColor: Colors.grey,
+              starCount:5,
+              onRatingChanged: (rating) => setState(
+                    () {
+                      this.rating = rating;
+                    },
+                  ),
+            ),
+          ]
           ),
           Row(
-            children:[TextField()]
+            children:[ TextFormField(validator: (value) {
+                  if (value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                  },
+                )
+              ]
             ),
-          Row(
+          Row(children:[
+              RaisedButton(
+                  onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        Scaffold.of(context).showSnackBar(SnackBar(content: Text('Processing Data')));
+                        }
+                    },
+                    child: Icon(Icons.check_circle),
+                    //Text("Confirmar"),
+                )
+           ] ),
+        ]
+      )
+    );
+  }*/
+
+
+
+
+
+
+
+/*
+Row(
+            children:[new StarRating(
+              size: 15.0,
+              rating: rating,
+              color: Colors.orange,
+              borderColor: Colors.grey,
+              starCount:5,
+              onRatingChanged: (rating) => setState(
+                    () {
+                      this.rating = rating;
+                    },
+                  ),
+            ),
+          ]
+          ),
+
+
+*/
+
+
+/*
               children:[FlatButton(
                 onPressed: () => {},
                 color: Colors.pink,
@@ -112,4 +248,9 @@ class TablaValoracion extends StatelessWidget{
 
     );
 }
-}
+}*/
+  
+
+
+
+
